ba30fed3f2d598e1890f9a5fda4d65c0b9974418
abeni-csa <abenezerhaymanot@gmail.com>
2025-03-29 14:07:36 +0300

    Grouping Configuration Values

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
-    println!("searcging for {query} in {file_path}");
-    let contests = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let config = parse_config(&args);
+
+    println!("searcging for {}", config.query);
+    println!("In file {}", config.file_path);
+    let contests =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("with text:\n {contests}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
 }
