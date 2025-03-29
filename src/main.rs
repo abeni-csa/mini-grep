ecb956385388b48166c8f40a0e47b4505fa9d6c7
abeni-csa <abenezerhaymanot@gmail.com>
2025-03-29 13:36:13 +0300

    Extracting the Argument Parser

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
     println!("searcging for {query} in {file_path}");
     let contests = fs::read_to_string(file_path).expect("Should have been able to read the file");
     println!("with text:\n {contests}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
 }
