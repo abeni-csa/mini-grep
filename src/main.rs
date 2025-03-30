5890f51caeb4ae58406b2979e9a7c42ebcc6d860
Abenezer Haymanot <143000984+abeni-csa@users.noreply.github.com>
2025-03-30 17:27:33 +0300

    Splitting Code into a Library Crate

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
-use std::env;
-use std::fs;
-use std::process;
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing the arguments{err}");
+        println!("Problem parsing the arguments: {err}");
         process::exit(1);
     });
 
     println!("searcging for {}", config.query);
     println!("In file {}", config.file_path);
-    run(config);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("no enough argumements passed try minigrep string path ");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application ERROR: {e}");
+        process::exit(1);
     }
-}
-
-fn run(config: Config) {
-    let contests =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("with text:\n {contests}");
 }
