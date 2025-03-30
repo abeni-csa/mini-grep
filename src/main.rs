d8bcbbe2c8003b7ddc076d5a73376d229cc63c7b
abeni-csa <abenezerhaymanot@gmail.com>
2025-03-30 17:22:30 +0300

    Split Code into a Library Crate lib.rs and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
-use std::{env, fs, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,30 +10,8 @@ fn main() {
 
     println!("searcging for {}", config.query);
     println!("In file {}", config.file_path);
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application ERROR: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("\nno enough argumements passed try minigrep string path ");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contests = fs::read_to_string(config.file_path)?;
-    println!("with text:\n {contests}");
-    Ok(())
 }
