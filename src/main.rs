7a705c683e12ee4b5e6104543bc77b3f426549bf
abeni-csa <abenezerhaymanot@gmail.com>
2025-03-30 12:52:41 +0300

    Extracting Logic from main and improve modularity

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,7 @@ fn main() {
 
     println!("searcging for {}", config.query);
     println!("In file {}", config.file_path);
-    let contests =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("with text:\n {contests}");
+    run(config);
 }
 
 struct Config {
@@ -24,10 +22,16 @@ struct Config {
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("no enough argumements passed \ntry minigrep string path ");
+            return Err("no enough argumements passed try minigrep string path ");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    let contests =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    println!("with text:\n {contests}");
 }
