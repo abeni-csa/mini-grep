7a6a1cc54ea1d6869aa9e35562c1562ea5699e41
Abenezer Haymanot <143000984+abeni-csa@users.noreply.github.com>
2025-04-01 11:27:15 +0300

    Writ Error Messages to Standard Error Instead of Standard Output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use minigrep::Config;
 use std::{env, process};
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("[!] Problem parsing the arguments: {err}");
         process::exit(1);
     });
-
     println!("[+] searcging for {}", config.query);
     println!("[+] In file {}", config.file_path);
     if let Err(e) = minigrep::run(config) {
