a6ccb77e6a339cb60f51e5cff397b5b1ec5f530d
Abenezer Haymanot <143000984+abeni-csa@users.noreply.github.com>
2025-04-01 11:26:27 +0300

    final step

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing the arguments: {err}");
+        eprintln!("[!] Problem parsing the arguments: {err}");
         process::exit(1);
     });
 
-    println!("searcging for {}", config.query);
-    println!("In file {}", config.file_path);
+    println!("[+] searcging for {}", config.query);
+    println!("[+] In file {}", config.file_path);
     if let Err(e) = minigrep::run(config) {
-        println!("Application ERROR: {e}");
+        eprintln!("[!] Application ERROR: {e}");
         process::exit(1);
     }
 }
