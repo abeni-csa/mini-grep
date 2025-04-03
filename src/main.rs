a57025ff7060adbcef5751187967947db26ba170
Abenezer Haymanot <143000984+abeni-csa@users.noreply.github.com>
2025-04-03 17:27:40 +0300

    Using the Returned Iterator Directly instade of pusing it to itrators

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use minigrep::Config;
 use std::{env, process};
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    //    let args: Vec<String> = env::args().collect();
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("[!] Problem parsing the arguments: {err}");
         process::exit(1);
     });
+
     println!("[+] searcging for {}", config.query);
     println!("[+] In file {}", config.file_path);
     if let Err(e) = minigrep::run(config) {
@@ -13,5 +15,3 @@ fn main() {
         process::exit(1);
     }
 }
-// we can improve our program later  after learing new rust feauters
-// but for now i learn so many new things
