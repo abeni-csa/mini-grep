ba9ccef96a6c992d6863ac2d7f41256d81774e12
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-04 11:06:06 +0300

    Making Code Clearer with Iterator Adapters in  search_case_sensitive function

--- src/lib.rs
+++ src/lib.rs
@@ -41,14 +41,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<_> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
+
 pub fn search_case_insensitive<'z>(query: &str, contents: &'z str) -> Vec<&'z str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
